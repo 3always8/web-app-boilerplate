4e8d0788c3e8d473b576a52f4bff415c4028b07c
ssong <3always8@gmail.com>
2023-09-16 15:36:04 +0900

    Do cors setting to communicate with frontend

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -47,13 +47,15 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'users.apps.UsersConfig',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -167,6 +169,9 @@ CORS_ALLOW_HEADERS = (
     'origin',
     'user-agent',
     'X-CSRFToken',
+    'X-CSRFTOKEN',
     'csrftoken',
     'x-requested-with',
 )
+
+CSRF_COOKIE_NAME = "XSRF-TOKEN"
