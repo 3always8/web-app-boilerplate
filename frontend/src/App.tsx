d15c308d46890eb16d4acc994500fc330169a449
ssong <3always8@gmail.com>
2023-09-16 15:37:35 +0900

    Test usecase of backend api

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import {getUsers} from './apis/users'
 function App() {
+  console.log(getUsers())
+
   return (
     <div className="App">
       <header className="App-header">
